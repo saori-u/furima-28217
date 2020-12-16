55a4d292b71e2ae733296bbd84e8c20c1f6eeb5c
kojimakei <kojimaruuuuu@gmail.com>
2020-12-16 14:37:21 +0900

    商品出品機能の実装

---
+++ app/javascript/item_price.js
@@ -0,0 +1,13 @@
+function count (){
+  const priceInput = document.getElementById("item-price");
+  priceInput.addEventListener("input", () => {
+    const inputValue = priceInput.value;
+    const addTaxDom = document.getElementById("add-tax-price");
+    addTaxDom.innerHTML =(Math.floor(inputValue * 0.1));
+    const sellProfit = document.getElementById("profit");
+    const value_result = inputValue * 0.1
+    sellProfit.innerHTML =(Math.floor(inputValue - value_result));
+  });
+}
+
+window.addEventListener('load', count);
---
+++ app/models/item.rb
@@ -0,0 +1,27 @@
+class Item < ApplicationRecord
+  belongs_to :user
+  has_one_attached :image
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :category
+  belongs_to_active_hash :condition
+  belongs_to_active_hash :postage
+  belongs_to_active_hash :prefecture
+  belongs_to_active_hash :day
+
+
+  validates :image,    presence: true
+  validates :name,    presence: true
+  validates :text,    presence: true
+  validates :price,   presence: true
+  validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999 }
+
+
+  validates :category_id, numericality: { other_than: 1 } 
+  validates :condition_id, numericality: { other_than: 1 } 
+  validates :postage_id, numericality: { other_than: 1 } 
+  validates :prefecture_id, numericality: { other_than: 1 } 
+  validates :day_id, numericality: { other_than: 1 } 
+
+end
+
