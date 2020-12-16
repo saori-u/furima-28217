779056086721c6f08fd9cf51aa2d1d4852afc0eb
kojimakei <kojimaruuuuu@gmail.com>
2020-12-16 23:28:44 +0900

    もし商品が１つも出品されていない場合、ダミー画像が表示される

--- app/models/item.rb
+++ app/models/item.rb
@@ -9,21 +9,20 @@ class Item < ApplicationRecord
   belongs_to_active_hash :prefecture
   belongs_to_active_hash :day
 
-
   with_options presence: true do
-   validates :image
-   validates :name
-   validates :text
-   validates :price
+    validates :image
+    validates :name
+    validates :text
+    validates :price
   end
 
   validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
 
   with_options numericality: { other_than: 1 } do
-  validates :category_id
-  validates :condition_id
-  validates :postage_id
-  validates :prefecture_id
-  validates :day_id
+    validates :category_id
+    validates :condition_id
+    validates :postage_id
+    validates :prefecture_id
+    validates :day_id
   end
 end
