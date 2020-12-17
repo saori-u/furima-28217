9c67f2a62bd61ec9882eba04839e2efbf1a4ccdb
kojimakei <kojimaruuuuu@gmail.com>
2020-12-17 16:56:55 +0900

    if文の条件分岐の改行を1行に変更。

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -26,7 +26,7 @@ class ItemsController < ApplicationController
   end
 
   def destroy
-    @item.destroy
+    @item.destroy if current_user.id == @item.user_id
     if current_user.id == @item.user_id
     redirect_to root_path
     end
