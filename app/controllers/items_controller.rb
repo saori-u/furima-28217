27bab466a31625b97b1671ccdc4d76e1cbdd4c84
kojimakei <kojimaruuuuu@gmail.com>
2020-12-17 14:10:36 +0900

    商品の編集と更新

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -14,6 +14,19 @@ class ItemsController < ApplicationController
   end
 
   def edit
+    @item = Item.find(params[:id])
+    unless current_user.id == @item.user_id
+      redirect_to root_path
+    end
+  end
+
+  def update
+     @item = Item.find(params[:id])
+     if @item.update(item_params)
+      redirect_to item_path(item.id)
+     else
+      render :edit
+     end
   end
 
   def destroy
@@ -31,7 +44,7 @@ class ItemsController < ApplicationController
   private
 
   def item_params
-    params.require(:item).permit(:name, :text, :category_id, :condition_id, :postage_id, :prefecture_id, :day_id, :price,
-                                 :image).merge(user_id: current_user.id)
+    params.require(:item).permit(:name, :text, :category_id, :condition_id, :postage_id, :prefecture_id, :day_id, :price,:image)
+    .merge(user_id: current_user.id)
   end
 end
