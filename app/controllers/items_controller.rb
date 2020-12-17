6997e9ebfbd605266594974e85be7b3fc4a9f961
kojimakei <kojimaruuuuu@gmail.com>
2020-12-17 15:16:47 +0900

    重複した@item = Item.find(params[:id])をまとめる。

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
+  before_action :set_items, only: [:show, :edit, :update]
 
   def index
     @items = Item.includes(:user).order('created_at DESC')
@@ -10,18 +11,15 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
   end
 
   def edit
-    @item = Item.find(params[:id])
     unless current_user.id == @item.user_id
       redirect_to root_path
     end
   end
 
   def update
-     @item = Item.find(params[:id])
      if @item.update(item_params)
       redirect_to item_path(@item.id)
      else
@@ -46,5 +44,9 @@ class ItemsController < ApplicationController
   def item_params
     params.require(:item).permit(:name, :text, :category_id, :condition_id, :postage_id, :prefecture_id, :day_id, :price,:image)
     .merge(user_id: current_user.id)
+  end
+
+  def set_items
+    @item = Item.find(params[:id])
   end
 end
